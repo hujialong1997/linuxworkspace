1c193843cc2bb94d164a8b9027c379c506203c2d
hujialong1997 <466126830@qq.com>
2018-04-08 18:44:44 -0700

    socket

--- cmdtestsocket.py
+++ cmdtestsocket.py
@@ -1,9 +1,16 @@
 import os
 import json
 import urllib2
+from socket import * 
+import sys
 score=0
+if len(sys.argv)!=3:
+    print "student number or student name  error!"
+    sys.exit()
 homepath=os.environ['HOME']
 jsondata={}
+jsondata["stuno"]=sys.argv[1]
+jsondata["stuname"]=sys.argv[2]
 #os.path.expanduser('$HOME')
 os.chdir(homepath)
 if os.path.exists("testdir"):
@@ -22,19 +29,19 @@ if os.path.isfile("passwd"):
     jsondata["passwd"]=1
 else:
     jsondata["passwd"]=0
-myurl="http://localhost"
-port="8888"
-route="login"
 j_data=json.dumps(jsondata)
-headers={'Content-Type':'application/json'}
-def jsonPost(url):
-    req=urllib2.Request(url,j_data,headers)
-    page=urllib2.urlopen(req)
-    res=page.read()
-    page.close()
-    return res
+def start_tcp_client(ip,port):
+    server_ip=ip
+    server_port=port
+    tcp_client=socket(AF_INET,SOCK_STREAM)
+    try:
+        tcp_client.connect((server_ip,server_port))
+    except error:
+        print 'fail to setup socket connection'
+    else:
+        tcp_client.sendall(j_data)
+    tcp_client.close()
+start_tcp_client("127.0.0.1",8888)
 print score
 print jsondata
 print j_data
-res=jsonPost("%s:%s/%s"%(myurl,port,route))
-print res
--- webservice1.py
+++ webservice1.py
@@ -1,6 +1,7 @@
 import web
 import socket
 #from outjson import index
+import os
 import json
 HOSTPORT='127.0.0.1',8888
 listen_socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
@@ -11,14 +12,21 @@ print 'Serving HTTP on port %s ...' %HOSTPORT[1]
 while True:
     client_connection,client_address = listen_socket.accept()
     request = client_connection.recv(1024)
-    ''' print request
-    http_response="""
+    '''with open(request,'r') as f:
+        lines=f.readlines()
+        last_line=lines[-1]
+    print last_line'''
+    http_response="""\
     HTTP/1.1 200 OK
-    Hello,World!
-    """
-    jsondata.GET()'''
+    Hello,World!"""
+    json_dict=json.dumps(request.strip())
+    #json.loads(request)
+    # if not os.path.exists(jsondata.txt):
+    with open("jsondata.txt",'a+') as f:
+        f.write(json.dumps(json_dict))
+        f.close
+    #jsondata.GET()
     #web.header('context-type','text/html')
-    
     client_connection.sendall(json.dumps({'one':'1'}).encode('utf-8'))
-    #ttp_response)
+    #client_connection.sendall(http_response)
     client_connection.close()
